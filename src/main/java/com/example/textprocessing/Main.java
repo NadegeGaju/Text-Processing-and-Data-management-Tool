bc4d454100b0c631458df96e0a94b51cdf6dd5c0
Gaju <nadege.gaju@amalitechtraining.org>
2024-07-23 14:25:26 +0200

    initial commit

---
+++ src/main/java/com/example/textprocessing/Main.java
@@ -0,0 +1,28 @@
+package com.example.textprocessing;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Text Processing Tool");
+
+        Label welcomeLabel = new Label("Welcome to Text Processing Tool");
+
+        VBox layout = new VBox(10);
+        layout.getChildren().addAll(welcomeLabel);
+
+        Scene scene = new Scene(layout, 400, 200);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
